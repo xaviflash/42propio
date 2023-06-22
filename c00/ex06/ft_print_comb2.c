6f19f9986fbf24fe52e10820dccb2989f5202506
Javier Zambrano Martin <jzambran@c3r12s5.42madrid.com>
2023-06-22 12:34:00 +0200

    2

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jzambran <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 12:02:06 by jzambran          #+#    #+#             */
+/*   Updated: 2023/06/07 16:06:26 by jzambran         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	num1;
+	int	num2;
+
+	num1 = 0;
+	while (num1 <= 98)
+	{
+		num2 = num1 + 1;
+		while (num2 <= 99)
+		{
+			ft_putchar((num1 / 10) + '0');
+			ft_putchar((num1 % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((num2 / 10) + '0');
+			ft_putchar((num2 % 10) + '0');
+			if (num1 != 98 || num2 != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			num2++;
+		}
+		num1++;
+	}
+}
